be8edfbf3d8739eb1e1448ae27d2147fe154d12f
taiyahorse <ly10069@126.com>
2022-08-30 22:17:56 +0800

    SSM初始版本，整合springboot、mybatis、jwt、redis、springmvc、druid

--- src/main/java/com/base/APP.java
+++ src/main/java/com/base/APP.java
@@ -1,13 +1,11 @@
 package com.base;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class APP {
     public static void main(String[] args) {
-        SpringApplication.run(APP.class);
+        SpringApplication.run(APP.class,args);
     }
 }
